1fe64319ab1df87a14955c7bcde5e9c327a5d75a
전지민HOME <iamjimin0722@gmail.com>
2024-05-16 21:28:17 +0900

    한글 깨지는 오류 수정

--- src/main/java/op/gg/project/league/controller/LeagueController.java
+++ src/main/java/op/gg/project/league/controller/LeagueController.java
@@ -5,16 +5,24 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -39,11 +47,13 @@ public class LeagueController {
 	@Autowired
 	private AccountService accountService;
 	
+	
+	// 메인화면에서 10명의 랭킹 정보만 가져옴
 	@CrossOrigin(origins = "http://localhost")
 	@GetMapping("/LeagueRanking")
 	public List<Summoner> LeagueRanking() {
 
-
+		System.out.println("asdad");
 		Account account = new Account();
 		Summoner summoner = new Summoner();
 		
@@ -84,6 +94,8 @@ public class LeagueController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println(leagueList);
 		
 		return leagueList;
 	}
@@ -132,16 +144,17 @@ public class LeagueController {
 	}
 	
 	// 소환사가 한 경기의 디테일정보
-	
 	@CrossOrigin(origins = "http://localhost")
 	@GetMapping("/matchDetail")
-	public String getMatchDetail(String matchId) {
+	public ResponseEntity<String> getMatchDetail(String matchId) {
 		
 		Account account = new Account();
 		
 		String apiUrl = "https://asia.api.riotgames.com/lol/match/v5/matches/"+matchId+"?api_key="+account.getApiKey();
 		StringBuffer response = new StringBuffer();
 		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8); // 또는 APPLICATION_JSON으로 설정하고 별도로 charset=UTF-8을 명시
 		try {
 			URL url = new URL(apiUrl);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -154,7 +167,7 @@ public class LeagueController {
 			    // retryAfterSeconds 만큼 대기 후 재시도 로직
 			}
 			System.out.println("Response Code : " + responseCode);
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String inputLine;
 			
 			if (responseCode != HttpURLConnection.HTTP_OK) {
@@ -176,9 +189,77 @@ public class LeagueController {
 			e.printStackTrace();
 		}
 		
+		System.out.println(response.toString());
+		
+//		return response.toString();
+		return new ResponseEntity<>(response.toString(), headers, HttpStatus.OK);
+	}
+	
+	
+	// 랭킹페이지에서 랭킹 목록 가져옴
+	@CrossOrigin(origins = "http://localhost")
+	@GetMapping("/getSoloRankInfo")
+	public  Summoner[] getSoloRankInfo(String tier, String division, int page) {
 		
 		
-		return response.toString();
+		System.out.println("데헷입니다");
+		
+		System.out.println(tier);
+		System.out.println(division);
+		System.out.println(page);
+		Account account = new Account();
+		String url = String.format("https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/%s/%s?page=%d&api_key=%s", 
+				tier, division, page, account.getApiKey());
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Summoner[]> response = restTemplate.getForEntity(url, Summoner[].class);
+
+		Summoner[] challengers = response.getBody();
+		System.out.println("challengers"+challengers.length);
+//		System.out.println(Arrays.copyOfRange(challengers, (page - 1) * 100, page * 100));
+		
+		/*
+		 * Account account = new Account(); Summoner summoner = new Summoner();
+		 * 
+		 * String apiUrl =
+		 * "https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/CHALLENGER/I?page="
+		 * +page+"&api_key="+account.getApiKey(); StringBuilder response = new
+		 * StringBuilder();
+		 * 
+		 * List<Summoner> leagueList = new ArrayList<Summoner>(); try { URL url = new
+		 * URL(apiUrl); HttpURLConnection conn = (HttpURLConnection)
+		 * url.openConnection(); conn.setRequestMethod("GET");
+		 * 
+		 * int responseCode = conn.getResponseCode();
+		 * System.out.println("Response Code : " + responseCode); BufferedReader in =
+		 * new BufferedReader(new InputStreamReader(conn.getInputStream())); String
+		 * inputLine;
+		 * 
+		 * 
+		 * while ((inputLine = in.readLine()) != null) { response.append(inputLine); }
+		 * in.close();
+		 * 
+		 * // Gson 라이브러리를 사용하여 JSON 응답에서 소환사 ID 추출 Gson gson = new Gson(); Type listType
+		 * = new TypeToken<List<Summoner>>(){}.getType(); List<Summoner>
+		 * leagueInfoAllList = gson.fromJson(response.toString(), listType);
+		 * 
+		 * 
+		 * // 처음 10명의 소환사 ID만 출력 for (int i = 0; i < Math.min(leagueInfoAllList.size(),
+		 * 100); i++) { summoner = leagueInfoAllList.get(i);
+		 * summonerService.summonerEncryptedSummonerId(summoner,summoner.getSummonerId()
+		 * ); accountService.accountPuuid(summoner,summoner.getPuuid());
+		 * leagueList.add(summoner); }
+		 * 
+		 * } catch (Exception e) { e.printStackTrace(); }
+		 */
+		int start = Math.max((page - 1) * 100, 0); // 시작 인덱스가 음수가 되지 않도록 보장
+	    int end = Math.min(page * 100, challengers.length); // 종료 인덱스가 배열 길이를 초과하지 않도록 보장
+	    
+	    // start가 end보다 크면, 올바르지 않은 범위이므로 빈 배열 반환
+	    if (start >= end) {
+	        return new Summoner[0];
+	    }
+//		return Arrays.copyOfRange(challengers, start, end);
+	    return response.getBody();
 	}
 	
 }

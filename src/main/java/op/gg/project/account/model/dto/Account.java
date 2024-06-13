9b06ee863929d4638065e38865a3e0bb20ae4cf1
JEONJIMIN <154505232+jimin1012@users.noreply.github.com>
2024-06-13 16:46:12 +0900

    Update Account.java

--- src/main/java/op/gg/project/account/model/dto/Account.java
+++ src/main/java/op/gg/project/account/model/dto/Account.java
@@ -15,6 +15,6 @@ import lombok.ToString;
 public class Account {
 
 	private String apiKey = "RGAPI-956baa76-3815-4961-adcf-7e0afb37b550"; // 라이엇 기본제공 개발용 키 24시간 마다 바꿔야함
-//	private String apiKey = "RGAPI-34d74300-b044-4e5d-b177-cd9bdd775bfa"; 기존 내가 받은 mini op.gg 개발 앱키
+//	private String apiKey = ""; 기존 내가 받은 mini op.gg 개발 앱키
 	private String url = "https://asia.api.riotgames.com";
 }
